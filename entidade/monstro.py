1eea0652af7ea843479f641ab34e0d63a09d10d1
Boreias <danielroberto95@hotmail.com>
2021-03-25 21:09:28 -0300

    Inserção de classes
    Classes:
    - Personagem
    - Jogador
    - Magia
    - Monstro
    - Arma

---
+++ entidade/monstro.py
@@ -0,0 +1,14 @@
+from personagem import Personagem
+import pygame
+
+class Monstro(Personagem):
+    def __init__(self, codigo: int, nome: str, forca: int, destreza: int, constituicao: int,
+                 inteligencia: int, sabedoria: int, carisma: int, imagem: pygame.image.load,
+                 ca: int, vida_maxima: int, tamanho: str,posicao: list, tipo: str,
+                 vida_atual = vida_maxima):
+        super().__init__(codigo, nome, forca, destreza, constituicao, inteligencia, sabedoria,
+                         carisma, imagem, ca, vida_maxima, tamanho, posicao, vida_atual)
+        self.__tipo = tipo
+
+    def atacar(self, personagem: Personagem):
+        pass
