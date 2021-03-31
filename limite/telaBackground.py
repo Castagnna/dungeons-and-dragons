0e90a2726cf7d303b2a91f196f3a5361bb72f089
Castagnna <daniel.c4stagna@gmail.com>
2021-03-30 21:10:02 -0300

    mais ajuste nas telas

--- limite/telaBackground.py
+++ limite/telaBackground.py
@@ -6,8 +6,4 @@ class TelaBackground(TelaGenerica):
     def __init__(self, controlador):
         super(TelaBackground, self).__init__(
             controlador=controlador,
-            titulo_da_tela="MENU BACKGROUND",
-            opcoes=(
-                (1, "Mostrar o mapa"),
-            )
         )
