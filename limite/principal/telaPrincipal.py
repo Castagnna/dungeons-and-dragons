662819c8696ae2991b5d3e772f6ce34e81141193
Castagna <daniel.c4stagna@gmail.com>
2021-05-08 22:00:55 -0300

    ajustando-layout

--- limite/principal/telaPrincipal.py
+++ limite/principal/telaPrincipal.py
@@ -12,17 +12,22 @@ class TelaPrincipal(TelaGenerica):
         self.init_components()
 
     def init_components(self):
-        sg.ChangeLookAndFeel('Reddit')
+        # sg.ChangeLookAndFeel("Reddit")
 
         layout = [
-            [sg.Button("Menu Jogador", key="JOGADOR")],
-            [sg.Button("Menu Monstro", key="MONSTRO")],
-            [sg.Button("Menu Arma", key="ARMA")],
-            [sg.Button("Menu Ataque Monstro", key="ATAQUE_MONSTRO")],
-            [sg.Button("Menu Background", key="BACKGROUND")],
-            [sg.Button("Menu Relatorio", key="RELATORIO")],
-            [sg.Button("Finaliza Programa", key="FINALIZAR")],
+            [sg.Button("Menu Jogador", key="JOGADOR", size=(20, 2))],
+            [sg.Button("Menu Monstro", key="MONSTRO", size=(20, 2))],
+            [sg.Button("Menu Arma", key="ARMA", size=(20, 2))],
+            [sg.Button("Menu Ataque Monstro", key="ATAQUE_MONSTRO", size=(20, 2))],
+            [sg.Button("Menu Background", key="BACKGROUND", size=(20, 2))],
+            [sg.Button("Menu Relatorio", key="RELATORIO", size=(20, 2))],
+            [sg.Button("Finaliza Programa", key="FINALIZAR", size=(20, 2))],
         ],
 
-        janela = sg.Window(title="Dungeons & Dragons").Layout(layout)
+        janela = sg.Window(
+            title="Dungeons & Dragons",
+            size=(300, 375),
+            resizable=True,
+            element_justification="center",
+            ).Layout(layout)
         super(TelaPrincipal, self).cria_janela(janela)
